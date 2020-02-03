af0d92ff0d293cef7200229f852d52e837286f49
Tom VanAntwerp <tom@tomvanantwerp.com>
2020-02-03 13:10:13 -0500

    Complete mappings and multi-cell titles concatenated

--- src/compileData.js
+++ src/compileData.js
@@ -11,14 +11,36 @@ const source = path.resolve(__dirname, 'data/facts-and-figures.xlsx');
 const destination = path.resolve(__dirname, 'data/data.json');
 const wb = XLSX.readFile(source);
 
+const concatRange = (range, sheet) => {
+  const cells = XLSX.utils.sheet_to_json(sheet, { header: 1, range, raw: false });
+  let values = [];
+
+  cells.forEach(row => {
+    row.forEach(cell => values.push(cell));
+  });
+
+  const cooncatenation = values.reduce((prev, curr) => {
+    return `${prev} ${curr.trim()}`
+  }, '');
+
+  return cooncatenation;
+};
+
 const mapValues = (table, sheet) => {
   data[table.sheetName] = {};
-  data[table.sheetName].values = XLSX.utils.sheet_to_json(sheet, { header: 1, range: table.data, raw: false });
-  data[table.sheetName].title = sheet[table.title] ? sheet[table.title].v : null;
-  data[table.sheetName].date = sheet[table.date] ? sheet[table.date].v : null;
-  data[table.sheetName].notes = sheet[table.notes] ? sheet[table.notes].v : null;
-  data[table.sheetName].source = sheet[table.source] ? sheet[table.source].v : null;
-  data[table.sheetName].footnotes = sheet[table.footnotes] ? sheet[table.footnotes].v : null;
+
+  const metadata = ['title', 'subtitle', 'date', 'notes', 'source'];
+
+  metadata.forEach(term => {
+    if (table[term]) {
+      data[table.sheetName][term] = table[term].indexOf(':') === -1
+        ? sheet[table[term]].v
+        : concatRange(table[term], sheet);
+    }
+  });
+  data[table.sheetName].footnotes = sheet[table.footnotes]
+    ? sheet[table.footnotes].v
+    : null;
 };
 
 const buildData = () => {
