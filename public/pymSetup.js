966b0a05ce306577840c8e793565a8a610ad9953
cpastre1 <csp@taxfoundation.org>
2024-05-22 15:03:06 -0400

    Embed fix take 2

--- public/pymSetup.js
+++ public/pymSetup.js
@@ -1,15 +1,7 @@
-window.addEventListener('load', function() {
+window.addEventListener("load", function () {
   const pymParent = new pym.Parent(
-    'facts-and-figures',
-    'https://facts-and-figures-web.netlify.com/index.html',
+    "facts-and-figures",
+    "https://facts-and-figures-web.netlify.app/",
     {}
   );
-
-  // attempt to tell iframe if we've navigated to specific table
-  // const targetIFrame = document.getElementById('facts-and-figures').firstChild;
-  // const queries = window.location.search;
-  // const params = new URLSearchParams(queries);
-  // const table = params.get('table');
-
-  // targetIFrame.postMessage(table);
 });
