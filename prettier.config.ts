08d731d46b2b43c20f1b68e8af018453c57cf88a
Tom VanAntwerp <tom@tomvanantwerp.com>
2026-02-09 16:14:52 -0500

    Add dev dependencies and configs

---
+++ prettier.config.ts
@@ -0,0 +1,16 @@
+import { type Config } from "prettier";
+
+const config: Config = {
+	plugins: ['@trivago/prettier-plugin-sort-imports'],
+	arrowParens: 'avoid',
+	printWidth: 80,
+	singleQuote: true,
+	useTabs: true,
+	semi: true,
+	importOrder: ['^[./]'],
+	importOrderSeparation: true,
+	importOrderSortSpecifiers: true,
+	importOrderCaseInsensitive: true,
+}
+
+export default config;
