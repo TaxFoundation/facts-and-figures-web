b280c079fa78a151315f3e86ab0de33d998d22b0
Tom VanAntwerp <tom@tomvanantwerp.com>
2026-02-09 17:39:48 -0500

    Extra config and updates

---
+++ packages/frontend/eslint.config.mjs
@@ -0,0 +1,24 @@
+import baseConfig from '../../eslint.config.mjs';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+
+export default [
+  ...baseConfig,
+  {
+    files: ['**/*.{jsx,tsx}'],
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+    },
+  },
+];
